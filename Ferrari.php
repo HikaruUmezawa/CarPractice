ed89884c0f345a76ee1547aea8bd5b5de2b37078
Hikaru Umezawa <hikaru.umezawa@tokyo.supersoftware.co.jp>
2021-04-16 09:51:14 +0900

    課題：Q2

--- Ferrari.php
+++ Ferrari.php
@@ -3,9 +3,45 @@
 
  class Ferrari extends Car
  {
+    public int $height;
+    private bool $isUpped;
+
     public function __construct()
     {
         parent::__construct(2,9.5,3000);
+        $this->height = 150;
+        $this->isUpped = false;
+    }
+
+    public function liftUp(){
+
+        //車高と加速度の計算
+        $this->height = $this->height + 40;
+        $this->speed = $this->speed * 0.8;
+        $this->isUpped = true;
+        echo "リフトアップしました。<br>";
+        return [$this->height,$this->speed];
+
+    }
+
+    public function liftDown(){
+
+        //リフトアップされていれば実行
+        if ($this->isUpped == true) {
+
+            //車高と加速度を元に戻す
+            $this->height = $this->height - 40;
+            $this->speed = $this->speed * 1.25;
+            $this->isUpped = false;
+           echo "リフトダウンしました。<br>";
+           return [$this->height,$this->speed];
+
+        }
+         else{
+
+             echo "リフトアップされていません。 <br>";
+
+        }
     }
  }
  
--- Main.php
+++ Main.php
@@ -11,9 +11,18 @@ class Main
         $nissan = new Nissan();
         $ferrari = new Ferrari();
 
+        //Q1
+        echo "--------Q1-------- <br>";
         echo "[Honda] 定員数：".$honda->capacity. "人　価格：".$honda->price."万円　加速度：".$honda->speed."m/s <br>"; 
         echo "[Nissan] 定員数：".$nissan->capacity. "人　価格：".$nissan->price."万円　加速度：".$nissan->speed."m/s <br>"; 
         echo "[Ferrari] 定員数：".$ferrari->capacity. "人　価格：".$ferrari->price."万円　加速度：".$ferrari->speed."m/s <br>"; 
+        
+        //Q2
+        echo "--------Q2-------- <br>";
+        echo "[Ferrari] 車高：".$ferrari->height."mm　加速度：".$ferrari->speed."m/s <br>"; 
+        $ferrari->liftUp();
+        echo "[Ferrari] 車高：".$ferrari->height."mm　加速度：".$ferrari->speed."m/s <br>"; 
+
     }
 }
 
